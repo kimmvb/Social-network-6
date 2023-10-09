751911fabe74c709383feb6f9304577acc2c7ecb
kimmvb <kimveb1@gmail.com>
2023-10-09 12:22:47 -0500

    Updates

--- src/lib/index.js
+++
@@ -1,7 +0,0 @@
-/* aqui exportaras las funciones que necesites */
-
-// eslint-disable-next-line import/no-mutable-exports
-export let userSession;
-export const saveUserSession = (user) => {
-  userSession = user;
-};
